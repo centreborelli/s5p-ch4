a0eac913cbfc071e4abf1ff3e47f7e5bcf2553f3
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2021-03-26 13:57:52 +0100

    -

---
+++ bin/wgets.sh
@@ -0,0 +1,2 @@
+wget --user=s5pguest --password=s5pguest "https://s5phub.copernicus.eu/dhus/odata/v1/Products('c9b29c59-c622-4d70-9f17-acad3144465f')/\$value" -O S5P_OFFL_L1B_RA_BD8_20200105T112716_20200105T130846_11550_01_010000_20200105T145137.nc
+wget --user=s5pguest --password=s5pguest "https://s5phub.copernicus.eu/dhus/odata/v1/Products('bbf13aaa-41f7-4b9b-93a3-f8a98a4292d9')/\$value" -O S5P_OFFL_L2__CH4____20200105T112716_20200105T130846_11550_01_010302_20200107T042409.nc
